290ac01aaf911fbdd777229a86eb2545d923f25d
David Mandia <david.mandia@yahoo.com>
2024-06-17 22:49:33 +0000

    printing sequences, suspect they are wrong

--- identify_indels.py
+++ identify_indels.py
@@ -2,14 +2,23 @@ import re
 import argparse
 import os
 import requests
+## Need to add genomics and trasncript position to cigar function
+## get genomics position of indels 
+## compare the two sequences 
+## generate VCF
 
-def identify_short_indels(cigar_string):
+import re
+
+def identify_short_indels(cigar_string, ):
     # Regular expression to extract length and operation from CIGAR string
     cigar_pattern = re.compile(r'(\d+)([MIDNSHP=X])')
     
-    # List to store short indels and mismatches
+    # List to store short indels and their positions
     short_indels = []
-    position = 0
+    
+    # Initialize positions
+    genomic_position = 0
+    transcriptomic_position = 0
     
     # Iterate through each operation in the CIGAR string
     for length, op in re.findall(cigar_pattern, cigar_string):
@@ -17,20 +26,19 @@ def identify_short_indels(cigar_string):
         length = int(length)
         
         if op == 'I' or op == 'D':
-            # Check if the length of the indel is less than or equal to 3
+            # Check if the length of the indel is less than 3
             if length < 3:
-                # Append the operation, length, and position to the list
-                short_indels.append((op, length, position))
+                # Append the operation, length, and both positions to the list
+                short_indels.append([op, length, genomic_position, transcriptomic_position])
         
-        # Update the position for match/mismatch
-        if op in 'M=X':
-            position += length
-        # Update the position to account for the indel or skipped region
-        elif op in 'DN':
-            position += length
+        # Update the transcriptomic position
+        if op in 'MIS=X':
+            transcriptomic_position += length
+        # Update the genomic position for match/mismatch
+        if op in 'M=XDN':
+            genomic_position += length
     
     return short_indels
-
 
 
 def get_ncbi_sequence(accession, start=None, end=None):
@@ -72,14 +80,19 @@ def parse_sam(file_path):
                 cigar = fields[5]
                 cigar_indels = identify_short_indels(cigar)
                 
-                # At least one short indel was found (op, length, position)
+                # At least one short indel was found (op, length, genomic_position, transcriptomic_position)
                 if len(cigar_indels) > 0:
-                    genomics_ref_sequence = get_ncbi_sequence(genome_ref, start=0, end=10)
-                    #print(genomics_ref_sequence[cigar_indels[2] -3: cigar_indels[2] + 3])
-                    print(genomics_ref_sequence)
-                    indels_obj[read_id] = []
-                    indels_obj[read_id].append(genome_ref)
-                    indels_obj[read_id].append(cigar_indels)
+                    transcript_sequence = fields[9]
+                    print(cigar_indels)
+                    for cigar_indel in cigar_indels:
+                        genomic_pos = cigar_indel[2]
+                        genomics_ref_sequence = get_ncbi_sequence(genome_ref, start=genomic_pos-5, end=genomic_pos+5)
+                        #print(genomics_ref_sequence[cigar_indels[2] -3: cigar_indels[2] + 3])
+                        transcript_sequence_positions = cigar_indel[3]
+                        print(genomics_ref_sequence, transcript_sequence[transcript_sequence_positions -5 : transcript_sequence_positions +5])
+                        indels_obj[read_id] = []
+                        indels_obj[read_id].append(genome_ref)
+                        indels_obj[read_id].append(cigar_indels)
 
                     
         return indels_obj

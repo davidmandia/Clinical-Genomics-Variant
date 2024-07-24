384df8acfe1033648fe1d7a67adde80924e61ae8
EC2 Default User <ec2-user@ip-172-31-65-68.ec2.internal>
2024-07-24 19:10:10 +0000

    changed README and locations

--- requirements/programm.sh
+++ requirements/programm.sh
@@ -1,4 +1,6 @@
+## This Bash script will run the complete database built for both genome version
+
 bash requirements/blast.sh
 python requirements/download_SAM_fromS3.py
-#bash requirements.sh
+bash requirements.sh
 bash requirements/scripts.sh

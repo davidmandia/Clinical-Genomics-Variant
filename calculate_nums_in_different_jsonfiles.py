d02a6daaac57c13f5f6db2c816c176bb17f53e85
Min <1454737096@qq.com>
2024-08-14 18:53:53 +0100

    count the number of variants appears in different files

---
+++ calculate_nums_in_different_jsonfiles.py
@@ -0,0 +1,61 @@
+import os
+import json
+from collections import defaultdict
+
+class VariantAnalyzer:
+    def __init__(self, directory, output_file):
+        self.directory = directory
+        self.output_file = output_file
+        self.variant_files = defaultdict(set)
+        self.dict_name = defaultdict(list) 
+
+    def _load_json(self, file_path):
+        with open(file_path, 'r') as file:
+            try:
+                data = json.load(file)
+                return data
+            except json.JSONDecodeError:
+                print(f"Error decoding JSON in file {file_path}")
+                return {}
+
+    def analyze(self):
+        """
+        Analyze the variants in all JSON files in the directory and count the number of files where each variant appears.
+        """
+        file_names = [f for f in os.listdir(self.directory) if f.endswith('.json')]
+        for file_name in file_names:
+            file_path = os.path.join(self.directory, file_name)
+            data = self._load_json(file_path)           
+            for variant in data.keys():
+                self.variant_files[variant].add(file_name)
+    
+    def output(self):
+        for variant, files in self.variant_files.items():
+            file_list = tuple(sorted(files))
+            self.dict_name[file_list].append(variant)
+
+    def write_variant_counts_to_file(self):
+        """
+        Write all variants and their statistics to an output file.
+        """
+        with open(self.output_file, 'w') as file:
+            file.write("file list,num of all the variants")
+            file.write('\n')
+            for files,variants in self.dict_name.items():
+                count = len(variants)
+                file.write(f'{files}, {count}\n')
+                file.write('\n')
+
+if __name__ == "__main__":
+    directory_GRCh37 = 'C:/Dissertation_project/Clinical-Genomics-Variant/LOVD_json/Select_variant/GRCh37'
+    output_file_GRCh37 = 'C:/Dissertation_project/Clinical-Genomics-Variant/LOVD_json/Select_variant/GRCh37_variant_counts.txt'
+    directory_GRCh38 = 'C:/Dissertation_project/Clinical-Genomics-Variant/LOVD_json/Select_variant/GRCh38'
+    output_file_GRCh38 = 'C:/Dissertation_project/Clinical-Genomics-Variant/LOVD_json/Select_variant/GRCh38_variant_counts.txt'
+    analyzer_GRCh37 = VariantAnalyzer(directory_GRCh37, output_file_GRCh37)
+    analyzer_GRCh37.analyze()
+    analyzer_GRCh37.output()   
+    analyzer_GRCh37.write_variant_counts_to_file()
+    analyzer_GRCh38 = VariantAnalyzer(directory_GRCh38, output_file_GRCh38)
+    analyzer_GRCh38.analyze()
+    analyzer_GRCh38.output()   
+    analyzer_GRCh38.write_variant_counts_to_file()
